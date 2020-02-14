2ea3f4559292cca4de6074af8ab482dcb6ca915a
maencina <mencina@cityuniversity.edu>
2020-02-13 21:02:47 -0800

    Removed dependency service call.

--- Mine/Mine/App.xaml.cs
+++ Mine/Mine/App.xaml.cs
@@ -16,9 +16,6 @@ namespace Mine
         {
             InitializeComponent();
 
-            // DependencyService.Register<MockDataStore>();
-            DependencyService.Register<DatabaseService>();
-
             // Call the Main Page to open
             MainPage = new MainPage();
         }
